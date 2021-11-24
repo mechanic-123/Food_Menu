dc23d6be2ebcabc54ffc9b766ba5b8b5d461de0f
deepika.an <deepika.an@BLR1-LHP-N73181>
2021-11-24 10:10:41 +0530

    Traffic impl has been updated

--- TMS_CRS/DAL/TrafficPoliceImpl.cs
+++ TMS_CRS/DAL/TrafficPoliceImpl.cs
@@ -16,7 +16,7 @@ namespace TMS_CRS.DAL
             db.TmOffences.Add(o);
             var res = db.SaveChanges();
             if (res == 1)
-                return (int) db.OffenceDetails.Max(x=>x.OffenceId);
+                return 1;
             return 0;
         }
         //traffic police can edit penalty status once the user pay penalty
