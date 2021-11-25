6713b3caa9b4d3aceb7aac56990f02b548e75c09
subhamthepirate <patrasubham82@gmail.com>
2021-11-25 17:27:45 +0530

    RTO Controller and View

--- UI_TMS/Controllers/ConsumeRTOController.cs
+++ UI_TMS/Controllers/ConsumeRTOController.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using TMS_CRS.Models;
 
 namespace UI_TMS.Controllers
 {
@@ -10,6 +12,27 @@ namespace UI_TMS.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+        public IActionResult AddOwner()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddOwner(TmOwnerdetail o)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:12850/api/RTO");
+                var postdata = client.PostAsJsonAsync<TmOwnerdetail>("AddUser", o);
+                postdata.Wait();
+                var result = postdata.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    ModelState.AddModelError("", "New Ower created");
+                }
+            }
             return View();
         }
     }
--- UI_TMS/Startup.cs
+++ UI_TMS/Startup.cs
@@ -46,7 +46,7 @@ namespace UI_TMS
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=ConsumeRTO}/{action=AddOwner}/{id?}");
             });
         }
     }
