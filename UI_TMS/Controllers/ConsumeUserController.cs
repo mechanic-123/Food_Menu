c9fb241b0c07ac06be808fb7f608d9ae8157b347
aditya149kr <aditya149kr@gmail.com>
2021-11-26 16:16:47 +0530

    Consuming User Controller(not completed)

--- UI_TMS/Controllers/ConsumeUserController.cs
+++ UI_TMS/Controllers/ConsumeUserController.cs
@@ -11,8 +11,34 @@ namespace UI_TMS.Controllers
 {
     public class ConsumeUserController : Controller
     {
+        readonly TMSDBContext db;
+        public ConsumeUserController(TMSDBContext db)
+        {
+            this.db = db;
+        }
         public IActionResult UserLogin()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UserLogin(string uname, string password)
+        {
+            uname = Request.Form["txtuser"];
+            password = Request.Form["txtpass"];
+            var data = (from user in db.TmUsermasters
+                        where (user.Username == uname && user.Password == password)
+                        select user.Rolename);
+            if (data != null)
+            {
+                if (Convert.ToString(data) == "RTO Officer")
+                    RedirectToAction("");
+                if (Convert.ToString(data) == "Traffic Police")
+                    RedirectToAction("");
+                if (Convert.ToString(data) == "Driver")
+                    RedirectToAction("");
+            }
+            else
+                ModelState.AddModelError("", "Invalid Username or Password");
             return View();
         }
         public IActionResult AddUser()
@@ -22,16 +48,24 @@ namespace UI_TMS.Controllers
         [HttpPost]
         public IActionResult AddUser(TmUsermaster u)
         {
-            using (var client = new HttpClient())
+            try
             {
-                client.BaseAddress = new Uri("http://localhost:28655/api/");
-                var postdata = client.PostAsJsonAsync<TmUsermaster>("User/AddUser", u);
-                postdata.Wait();
-                var res = postdata.Result;
-                if (res.IsSuccessStatusCode)
-                    ModelState.AddModelError("", "New User Added");
-                else
-                    ModelState.AddModelError("",res.ToString());
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:12850/api/");
+                    var postdata = client.PostAsJsonAsync<TmUsermaster>("User/AddUser", u);
+                    postdata.Wait();
+                    var res = postdata.Result;
+                    if (res.IsSuccessStatusCode)
+                        ModelState.AddModelError("", "New User Added");
+                }
+            }
+            catch (AggregateException err)
+            {
+                foreach (var errInner in err.InnerExceptions)
+                {
+                    ModelState.AddModelError("", errInner.ToString()); //this will call ToString() on the inner execption and get you message, stacktrace and you could perhaps drill down further into the inner exception of it if necessary 
+                }
             }
             return View();
         }
